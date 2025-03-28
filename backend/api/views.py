e145f791de44a86a34e7420f7184ef5238636b43
czr_dev <cezarmutran@gmail.com>
2025-03-28 01:45:02 -0300

    feat: implement user profile model and serializers, update API endpoints

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,33 +1,19 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import UserSerializer, NoteSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from .models import Note
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-class NoteListCreate(generics.ListCreateAPIView):
-    serializer_class = NoteSerializer
-    permission_classes = [IsAuthenticated]
+from .serializers.register_serializer import RegisterSerializer
+from .serializers.user_serializer import UserSerializer
 
-    def get_queryset(self):
-        user = self.request.user
-        return Note.objects.filter(author=user)
-    
-    def perform_create(self, serializer):
-        if serializer.is_valid():
-            serializer.save(author=self.request.user)
-        else:
-            return (serializer.errors)
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]  # Permite que usuários não autenticados acessem
 
-class NoteDelete(generics.DestroyAPIView):
-    serializer_class = NoteSerializer
-    permission_classes = [IsAuthenticated]
+    def create(self, request, *args, **kwargs):
+        register_serializer = self.get_serializer(data=request.data)
+        register_serializer.is_valid(raise_exception=True)
+        user = register_serializer.save()  # Cria o User e o Profile
 
-    def get_queryset(self):
-        user = self.request.user
-        return Note.objects.filter(author=user)
-
-class CreateUserView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [AllowAny]
+        # Se quiser retornar todos os dados, inclusive do Profile:
+        user_data = UserSerializer(user).data
+        return Response(user_data, status=status.HTTP_201_CREATED)
